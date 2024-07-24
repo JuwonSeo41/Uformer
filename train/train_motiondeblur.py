5d9b17e29d59dd0f0d4f0ae355834a561d3cf46f
JuwonSeo41 <justin412880@gmail.com>
2024-07-24 16:54:43 +0900

    Update train_motiondeblur.py

--- train/train_motiondeblur.py
+++ train/train_motiondeblur.py
@@ -126,12 +126,12 @@ criterion = CharbonnierLoss().cuda()
 ######### DataLoader ###########
 print('===> Loading datasets')
 img_options_train = {'patch_size':opt.train_ps}
-train_dataset = get_training_data(opt.train_blur_dir, img_options_train)
+train_dataset = get_training_data(opt.train_blur_dir, opt.train_target_dir, img_options_train)
 train_loader = DataLoader(dataset=train_dataset, batch_size=opt.batch_size, shuffle=True, 
         num_workers=opt.train_workers, pin_memory=False, drop_last=False)
 
 img_options_val = {'patch_size':opt.val_ps}
-val_dataset = get_validation_deblur_data(opt.val_blur_dir, img_options_val)
+val_dataset = get_validation_deblur_data(opt.val_blur_dir, opt.val_target_dir, img_options_val)
 
 val_loader = DataLoader(dataset=val_dataset, batch_size=opt.batch_size, shuffle=False, 
         num_workers=opt.eval_workers, pin_memory=False, drop_last=False)
